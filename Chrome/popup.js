0e42b5df08c5dac66c4066ba3ef93bb71d69a838
Aidan Horton <Aidan.Horton@malvern.com>
2020-01-27 13:33:41 +0000

    Add custom logo to popup

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -1,6 +1,4 @@
 function toggleDark () {
-	let body = document.getElementById("main-body");
-				
 	if (checkBox.checked == true) {
 		body.style.backgroundColor = "#262626";
 		chrome.storage.sync.set({ "darkMode": "on" });
@@ -16,19 +14,30 @@ function toggleDark () {
 	}
 }
 
-function setTransitionFlag () {
+function setTransitionFlags () {
 	let checkBoxLabel = document.getElementById("toggle");
 	checkBoxLabel.classList.add("clicked");
+
+	body.classList.add("body-transition");
 }
+
+let body = document.getElementById("main-body");
+
+var image = document.getElementById("logo");
+image.src = chrome.runtime.getURL("images/EcosiaLogo48.png");
 
 let checkBox = document.getElementById("switch");
 checkBox.onclick = toggleDark;
+
 chrome.storage.sync.get(["darkMode"], function(items){
     if (items["darkMode"] == 'on') {
 		checkBox.checked = true;
+		body.style.backgroundColor = "#262626";
+		
 	} else {
 		checkBox.checked = false;
+		body.style.backgroundColor = "#F2F2F2";
 	}
 });
 
-window.setTimeout(setTransitionFlag, 100);
+window.setTimeout(setTransitionFlags, 100);
