e55313f53a4788d2de898db7b21d9a03ad8ddbb9
Aidan Horton <Aidan.Horton@malvern.com>
2020-01-27 12:38:28 +0000

    Add seamless CSS injection + browser storage

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -3,17 +3,32 @@ function toggleDark () {
 				
 	if (checkBox.checked == true) {
 		body.style.backgroundColor = "#262626";
+		chrome.storage.sync.set({ "darkMode": "on" });
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			chrome.tabs.sendMessage(tabs[0].id, {mode: "on"});
 		});
 	} else {
 		body.style.backgroundColor = "#F2F2F2";
+		chrome.storage.sync.set({ "darkMode": "off" });
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			chrome.tabs.sendMessage(tabs[0].id, {mode: "off"});
 		});
 	}
 }
 
+function setTransitionFlag () {
+	let checkBoxLabel = document.getElementById("toggle");
+	checkBoxLabel.classList.add("clicked");
+}
+
 let checkBox = document.getElementById("switch");
 checkBox.onclick = toggleDark;
-checkBox.checked = true;
+chrome.storage.sync.get(["darkMode"], function(items){
+    if (items["darkMode"] == 'on') {
+		checkBox.checked = true;
+	} else {
+		checkBox.checked = false;
+	}
+});
+
+window.setTimeout(setTransitionFlag, 100);
