46f4965f896388426af3ed02479c44cbbcacbee9
Aidan Horton <Aidan.Horton@malvern.com>
2020-01-28 17:25:20 +0000

    Message all tabs when dark mode turned on/off

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -2,14 +2,18 @@ function toggleDark () {
 	if (checkBox.checked == true) {
 		body.style.backgroundColor = "#262626";
 		chrome.storage.sync.set({ "darkMode": "on" });
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, {mode: "on"});
+		chrome.tabs.query({}, function(tabs) {
+			for (var i=0; i<tabs.length; ++i) {
+				chrome.tabs.sendMessage(tabs[i].id, {mode: "on"});
+			}
 		});
 	} else {
 		body.style.backgroundColor = "#F2F2F2";
 		chrome.storage.sync.set({ "darkMode": "off" });
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, {mode: "off"});
+		chrome.tabs.query({}, function(tabs) {
+			for (var i=0; i<tabs.length; ++i) {
+				chrome.tabs.sendMessage(tabs[i].id, {mode: "off"});
+			}
 		});
 	}
 }
