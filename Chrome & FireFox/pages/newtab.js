722db964313eaa1c5e3ce8b522ca5738dc6bcacb
Aidan Horton <aidan.horton@virginmedia.com>
2020-11-09 16:47:04 +0000

    Add light mode toggle to newtab page

--- Chrome & FireFox/pages/newtab.js
+++ Chrome & FireFox/pages/newtab.js
@@ -42,3 +42,45 @@ document.addEventListener('DOMContentLoaded', function() {
 		}
     });
 });
+
+function toggleStyle(message, sender, sendResponse) {
+	if (message.action == 'on') {
+		var element = document.getElementById('EcosiaLightMode');
+		element.parentElement.removeChild(element);
+	}
+	else if (message.action == 'off') {
+		document.getElementsByTagName("head")[0].appendChild(style);
+	}
+}
+
+chrome.storage.local.get(["darkMode"], injectOnLoad);
+
+function injectOnLoad(items){
+    if (items["darkMode"] === 'off') {
+		(document.body || document.head || document.documentElement).appendChild(style);
+	}
+}
+
+chrome.runtime.onMessage.addListener(toggleStyle);
+var style = document.createElement('style');
+style.id = "EcosiaLightMode";
+style.className = "EcosiaLightMode";
+style.type = "text/css";
+style.textContent = `
+body, form, input, a, button, .nav-menu-group, #dropdown-list {
+	background: #FFF !important;
+	border-color: #E0E0E0 !important;
+	color: #4A4A4A !important;
+}
+
+a:hover, button:hover {
+	background: #E0E0E0 !important;
+}
+
+.logo:hover {
+	background: #FFF !important;
+}
+
+.logo-text {
+	fill: #4B4A4B;
+}`;
