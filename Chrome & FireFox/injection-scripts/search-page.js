60148f7c31f06c7c0f5768cb5b772bc8a18ceeda
Muhamad Fajar <mfajar0910@gmail.com>
2021-07-23 07:25:03 +0700

    Fix dark theme for new Ecosia search result

--- Chrome & FireFox/injection-scripts/search-page.js
+++ Chrome & FireFox/injection-scripts/search-page.js
@@ -10,11 +10,11 @@ function injectOnLoad(items){
 // Subscribe to other necessary events.
 document.addEventListener('DOMContentLoaded', changeStyleImportance, false);
 chrome.runtime.onMessage.addListener(toggleStyle);
- 
+
 // Moves style tag to the head once the document has loaded.
 function changeStyleImportance(){
 	document.removeEventListener('DOMContentLoaded', changeStyleImportance, false);
-	
+
 	var darkModeElement = document.getElementById('EcosiaDarkMode');
 	if (darkModeElement != null) {
 		document.getElementsByTagName('head')[0].appendChild(darkModeElement);
@@ -39,412 +39,160 @@ style.type = "text/css";
 style.textContent = `/* ------------------------------------------------------- */
 /* SEARCH ENGINE - The main Ecosia search engine & results */
 /* ------------------------------------------------------- */
-
-/* Change 'applies to' so that it only applies to results */
-
-
-/* MAIN BACKGROUND */
-body {
-    background: var(--dark-background);
-}
-.search-header, .navbar-row, .results-wrapper {
+/* LOGO & MAIN STYLES */
+/* Main */
+.main-header__content, .layout__content {
     background: var(--dark-background) !important;
 }
-
-/* Background fix for 'Add Ecosia to Chrome' button */
-.col-lg-4 .margin-top-base {
-    background: var(--dark-background);
+/* Logo */
+.logo__icon > g > path:nth-child(2) {
+    fill: white !important;
 }
-/* ---------- */
-
-
-/* LOGO & SEARCH BAR */
-/* Normal logo */
-.logo-anchor path:nth-child(2) {
-    fill: white;
-}
-
 /* Search bar */
-.search-box .search-form-field {
-    background: #292929;
-}
-.search-form input {
+.search-form, input {
+    background: #292929 !important;
     color: var(--text) !important;
 }
-.search-form-button:focus svg path {
-    fill: #767676;
+/* Search suggestions */
+.search-form__suggestions-list {
+    background: var(--form) !important;
 }
-.search-form-button:hover svg path {
-    fill: #adadad;
-}
-
-/* Typeahead suggestions */
-.typeahead {
-    background: var(--form);
-    box-shadow: 0 0px 5px 0 rgba(255, 255, 255, 0.15), 0 0px 4px 0 rgba(255, 255, 255, 0.15);
-}
-.typeahead a {
-    color: var(--text);
-}
-.typeahead a:hover {
-    background: var(--lighter-background);
-    color: var(--text);
-}
-.typeahead a:visited {
-    color: #8d8d8d;
-}
-/* ---------- */
-
-
-/* NAVBAR */
-/* Main navbar style */
-.navbar-container a, .navbar-container a:visited {
-    color: var(--text);
-}
-.navbar-container a:hover {
-    color: #36acb8;
-}
-.navbar-container button {
-    color: var(--text);
-}
-
-/* Navbar dropdowns */
-.icons-dropdown-list li:hover {
-    background: var(--lighter-background);
-}
-.icons-dropdown-list li:hover a, .icons-dropdown-list a:focus, .icons-dropdown-list a:active {
-    color: var(--text);
-}
-[data-track-id="more_wikipedia"] path, [data-track-id="more_amazon"] path {
-    fill: var(--text);
-}
-
-/* Filters & settings */
-.flags-dropdown {
-    color: var(--text);
-}
-.flags-dropdown-chosen {
-    background: var(--form);
-}
-.flags-dropdown li:hover {
-    background: var(--lighter-background);
-}
-.filter-list, .filter-list > li:focus, .filter-list a, .filter-trigger {
-    background: var(--form);
+.search-form__suggestions-list a {
     color: var(--text) !important;
 }
-.filter-list a:hover, .filter-list .active a {
+.suggestion-item.selected {
     background: var(--lighter-background) !important;
 }
-.filter-trigger:hover {
-    color: #8d8d8d !important;
+/* Navigation bar */
+.search-navigation__item a {
+    color: var(--text) !important;
 }
-/* ---------- */
-
-/* MENU BUTTONS */
-/* Tree-counter, menu main styling */
-.search-header-menu-group button {
+.search-navigation__item > .tab > a:focus, .search-navigation__item > .tab > a:hover, .search-navigation__item:nth-child(2) > .tab > a {
+    color: #36acb8 !important;
+}
+/* Filters */
+.search-filters, .main-header {
+    background: var(--dark-background);
+}
+/* Query chips */
+.query-expansions__item {
+    background: var(--form) !important;
+}
+.chip__text {
+    color: var(--text) !important;
+}
+.chip__text > mark {
+    color: #888;
+}
+.query-expansions__button {
+    background: linear-gradient(90deg, hsla(0, 0%, 100%, 0), hsla(0, 0%, 12%, .8) 42%, var(--dark-background) 74%) !important;
+}
+/* Search results */
+.mainline__result {
     background: var(--dark-background) !important;
+    color: var(--text); 
 }
-.search-header-menu-group button:hover {
-    background: var(--lighter-background) !important;
+.result-title {
+    color: var(--link) !important;
 }
-.notifications-center button:hover path, .notifications-center.is-open path, .search-header-dropdown button:hover path, .search-header-dropdown.is-open path {
-    fill: var(--text) !important;
+.result-title a, .result-title a:hover, .result-title a:focus {
+    color: var(--link) !important;
 }
-.pill-text {
-    color: var(--text) !important;
-}
-.pill {
-    background: var(--main-background-color) !important;
-}
-
-/* Tree-counter popover */
-#treeCountPopover {
-    background: var(--form);
-    border: 1px solid;
-    color: var(--text);
-}
-
-/* Notifications center */
-.notifications-center-container {
-    background: var(--form);
-    border: 1px solid;
-}
-.notifications-item, .notifications-item:active, .notifications-item:focus, .notifications-item:visited {
-    background: var(--form);
-    color: var(--text) !important;
-}
-.notifications-item:hover {
-    background: var(--lighter-background);
-    color: var(--text);
-}
-.dropdown-footer-simple {
-    background: var(--form);
-}
-
-/* Dropdown menu */
-.dropdown-menu {
-    background: var(--form);
-    border-bottom: 1px solid;
-    border-left: 1px solid;
-    border-right: 1px solid;
-}
-.dropdown-link {
-    color: var(--text) !important;
-}
-.dropdown-item:hover {
-    background: var(--lighter-background);
-}
-/* ---------- */
-
-
-/* RESULTS */
-/* Main text result color */
-.result-title, .result-title:hover, .result-title:focus {
-    color: var(--link);
-}
-.result-title:visited {
-    color: var(--link-visited);
+.result-title a:visited {
+    color: var(--link-visited) !important;
 }
 .result-url, .result-url:hover, .result-url:visited, .result-url:focus {
-    color: var(--link-green);
+    color: var(--link-green) !important;
 }
-.result-snippet, .query-context-text, .no-results {
+.result__source, .result__source:hover, .result__source:visited, .result__source:focus {
+    color: var(--link-green) !important;
+}
+.result__description {
     color: var(--text);
 }
-
-/* Text results extra media */
-.card-web .result-media .result-url {
-    background: var(--form);
-    border: 1px solid var(--lighter-background);
+.results-info {
+    border-color: var(--border) !important;
 }
-.result-media .thumbnail-title, .result-media-video .thumbnail-title:hover {
-    color: var(--link);
+/* Loading button */
+.loading-button {
+    background: var(--dark-background) !important;
 }
-.result-media .result-url:visited .thumbnail-title {
-    color: var(--link-visited);
-}
-.result-media .thumbnail-publisher {
-    color: var(--link-green);
-}
-.callout-with-image {
-	background-color: var(--form) !important;
-	color: var(--text) !important;
-	border: 1px solid var(--border);
-}
-[title="Wikipedia"] path {
-    fill: #CCCCCC !important;
-}
-
-/* Extra results buttons */
-.result-media .slider-control {
-    background: #272727;
-    box-shadow: 0 0px 5px 0 rgba(255, 255, 255, 0.15), 0 0px 3px 0 rgba(255, 255, 255, 0.15);
-}
-.icon-button:hover {
+.loading-button__content {
     color: var(--text) !important;
+}
+/* Country dropdown */
+.dropdown {
+    background: var(--form) !important;
+    color: var(--text) !important;
+    border: 1px solid;
+}
+.dropdown li:hover {
     background: var(--lighter-background);
 }
-.icon-button:focus {
-    color: #828282;
+/* Search filters */
+.search-filters button {
+    background: var(--dark-background) !important;
+    color: var(--text) !important;
 }
-
-/* More results links */
-.more-results-link, .more-results-link:hover, .more-results-link:visited, .more-results-link:focus {
-    color: var(--link);
+.search-filters button:hover {
+    color: #949494 !important;
 }
-.more-results-link:visited {
-    color: var(--link-visited);
+/* Navbar dropdowns (maps, more) */
+.dropdown li:hover {
+    background: var(--lighter-background) !important;
 }
-
-/* Results footer */
-.pagination-button:hover, .pagination-button.active, .small-footer-link:hover {
-    color: var(--text);
+[data-track-id="more_wikipedia"] path, [data-track-id="more_amazon"] path {
+    fill: white;
 }
-
-/* Image results */
-.image-result {
-    border-color: var(--dark-background) !important;
+/* Pill buttons */
+.pill {
+    background: var(--dark-background) !important;
 }
-
-/* Climate impact */
-.entity-extra {
-    color: var(--text);
+.personal-counter__text {
+    color: var(--text) !important;
 }
-.entity-extra::before {
-    border-color: var(--border);
+.icon-button:hover, .icon-button:focus {
+    background: var(--lighter-background) !important;
 }
-/* ---------- */
-
-
-/* INLINE WIDGETS */
-.card-news li, .card-videos li {
-    background: var(--form);
-    border: 1px solid;
+.icon-button:hover path, .icon-button:focus path {
+    fill: var(--text) !important;
 }
-.widget-container {
-    border: 1px solid;
+.notification-pill:hover path, .notification-pill:focus path {
+    fill: var(--text) !important;
 }
-.widget-container div {
-    background: var(--form);
-    color: var(--text);
-}
-
-/* Flights */
-.card-flights {
-    background: var(--form);
-    border: 1px solid;
-}
-.card-flights div, .card-flights input {
-    background: var(--form) !important;
-    color: var(--text);
-}
-.bpk-button, .ReferralButton__cta___2Yxt0 {
+/* Notifications dropdown */
+.notifications-dropdown a {
     background: var(--form) !important;
     color: var(--text) !important;
 }
-
-/* Polluter */
-.highlighted-domain-hint-wrapper .popover {
-    border: 1px solid !important;
-    background: #272727 !important;
+.notifications-dropdown a:hover {
+    background: var(--lighter-background) !important;
 }
-.highlighted-domain-hint-wrapper .popover-text {
+/* Hamburger dropdown */
+.main-nav-menu li:hover {
+    background: var(--lighter-background) !important;
+}
+/* No results */
+.search-no-results {
     color: var(--text);
 }
-/* ---------- */
-
-
-/* MOBILE */
-/* Main results */
-.mainline-results .result, .results-page-stretcher {
-    background: var(--dark-background);
-}
-.search-form-addon .search-form-addon-button {
-    background: var(--dark-background);
+/* Mobile mode */
+.panel__content {
+    background: var(--form) !important;
     color: var(--text);
 }
-
-/* Hamburger & tree counter */
-.popover-mobile {
-    background: var(--form);
-    color: var(--text);
-}
-.popover-treecounter {
-    border: 1px solid;
-}
-.offcanvas-menu-header {
-    background: var(--form);
-}
-
-/* Maps & more */
-.card-desktop .card-mobile {
-    background: var(--dark-background);
-}
-.navbar-item.hidden-desktop .navbar-link {
-    color: var(--text);
-}
-.container.hidden-desktop .row div {
-    background: var(--form);
-}
-#moreLinks.hidden-desktop li {
-    background: var(--form);
-}
-#moreLinks.hidden-desktop li:hover {
+.list-item:hover {
     background: var(--lighter-background);
 }
-#moreLinks.hidden-desktop a {
-    color: var(--text);
-}
-#mapLinks.hidden-desktop li {
+.settings-dropdown__content {
     background: var(--form);
 }
-#mapLinks.hidden-desktop li:hover {
-    background: var(--lighter-background);
+.modal-dropdown__content {
+    background: var(--form) !important;
 }
-#mapLinks.hidden-desktop a {
-    color: var(--text);
-}
-.navbar-item-divider::before {
-    border-color: var(--border);
-}
-
-/* Filters & settings */
-.hidden-desktop .flags-dropdown-content {
-    color: var(--text);
-}
-.hidden-desktop .flags-dropdown-content li:hover {
-    background: var(--lighter-background);
-}
-/* ---------- */
-
-
-/* ADVERTISEMENTS */
-/* Sidebar ads */
-.entity {
-    border: 1px solid;
-    background: var(--form);
-}
-.entity-disambiguation-title, .entity-disambiguation-title:hover, .entity-disambiguation-title:focus, .entity-disambiguation-title:active {
-    color: var(--link);
-}
-.entity-description-more, .entity-description-more:hover, .entity-description-more:focus, .entity-description-more:active {
-    color: var(--link);
-}
-.entity-disambiguation-title:visited, .entity-description-more:visited {
-    color: var(--link-visited);
-}
-.entity-disambiguation-text, .entity-disambiguation-headline, .entity-title, .entity p {
-    color: var(--text);
-}
-.card-top {
-    background: var(--darker-background);
-}
-
-/* Sidebar shop */
-.sidebar-product-ad-container {
-    border: 1px solid;
-    background: var(--form);
-}
-.sidebar-product-ad-title, .sidebar-product-ad-title:hover, .sidebar-product-ad-title:focus, .sidebar-product-ad-title:active {
-    color: var(--link);
-}
-.sidebar-product-ad-title:visited {
-    color: var(--link-visited);
-}
-.sidebar-product-ad-seller {
-    color: var(--link-green);
-}
-
-/* Inline */
-.card-productads {
-    background: var(--dark-background);
-}
-.product-ad-heading {
-    color: var(--text);
-}
-.product-ad li {
-    background: var(--form);
-    border: 1px solid;
-}
-.thumbnail-extra {
-    color: var(--text);
-}
-/* ---------- */
-
-
-/* MISCELLANEOUS */
-/* Install add-on banner Chrome */
-.serp-cta-wrapper {
-    background: var(--form);
-    color: var(--text);
-}
-
 /* ----------------------------------------------------------------------- */
 /* UNIVERSAL CODE - Code used between pages -> scrollbar, variables etc... */
 /* ----------------------------------------------------------------------- */
-
 * {
     border-color: var(--border) !important; 
     scrollbar-color: #3F3F3F #1C1E1F;
@@ -460,7 +208,6 @@ body {
 :focus {
     outline: 0;
 }
-
 /* Logo */
 [aria-label="Ecosia logo"] path:nth-child(2) {
     fill: white;
@@ -468,12 +215,10 @@ body {
 .land-background path {
     fill: var(--dark-background);
 }
-
 /* Footer */
 footer {
     background: var(--form) !important;
 }
-
 /* Scrollbar Chrome */
 ::-webkit-scrollbar {
     width: 15px;
@@ -487,7 +232,6 @@ footer {
 ::-webkit-scrollbar-thumb {
     background: #626262;
 }
-
 ::-moz-selection, ::selection {
     background: #696969;
 }`;
